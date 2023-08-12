451b64d535b4caa72351adb38e8181350d6793d6
Tarek-Abdo <tarekk.abdo22.gmail.com>
2023-08-12 04:45:41 +0300

    added succesfully

---
+++ index.js
@@ -0,0 +1,27 @@
+ScrollReveal({ distance: "60px", duration: 1500, delay: 400 });
+ScrollReveal().reveal(".main-title, .section-title", {
+  delay: 200,
+  origin: "left",
+});
+ScrollReveal().reveal(".sec-01 .image, .info", {
+  delay: 300,
+  origin: "bottom",
+});
+ScrollReveal().reveal(".text-box", {
+  delay: 400,
+  origin: "right",
+});
+ScrollReveal().reveal(".media-icons i", {
+  delay: 400,
+  origin: "bottom",
+  interval: 200,
+});
+ScrollReveal().reveal(".sec-02 .image, .sec-03 .image", {
+  delay: 400,
+  origin: "top",
+});
+ScrollReveal().reveal(".media-info li", {
+  delay: 500,
+  origin: "left",
+  interval: 200,
+});
